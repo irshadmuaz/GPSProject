9c868501bac4a82e552a286e7d89381064be089a
Christopher Brant <cbrant@g.clemson.edu>
2018-05-23 12:58:19 -0400

    updated nvsConv.sh

--- nvsData/nvsConv.sh
+++ nvsData/nvsConv.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # nvsConv.sh
-# Finalized on 5/18/18
+# Finalized on 5/23/18
 # Christopher Brant
 # This bash script is used to automate
 # the conversion of files from .nvs to .obs and .nav
+# and remove possible whitespace lines in the .obs file
 
 # .nvs file is the first argument
 NVSFILE=$1
