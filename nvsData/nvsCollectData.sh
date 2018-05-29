c6b7b05a053f42dd2d8c4c9757e6daf545df62f1
Christopher Brant <cbrant@g.clemson.edu>
2018-05-29 10:36:09 -0400

    test version of nvsCollectData.sh is written

--- nvsData/nvsCollectData.sh
+++ nvsData/nvsCollectData.sh
@@ -21,16 +21,25 @@ CURDIR=$(pwd)
 # name of current day's data directory
 DAYDIR="${TODAY}-data"
 
+# make the current day's directory if not already created
+mkdir -p $CURDIR/nvsData/$DAYDIR
 
+# edit the timeout value to 20m when fully working
+timeout 5m cat /dev/ttyAMA0 > $CURDIR/nvsData/$DAYDIR/$NVSFILE
 
+# sleep for 30s to ensure the file has correctly been written fully
+sleep 30s
 
-
-
+# convert the nvs binary file and save the .obs and .nav files to the correct location
 $CURDIR/nvsData/convbin -r nvs -o $OBSFILE -n $NAVFILE -d $CURDIR/nvsData/$DAYDIR -v 2.10 -od -os $CURDIR/nvsData/$DAYDIR/$NVSFILE
 
-# the next 3 commands ensure there are no white space lines in .obs file
+# remove whitespace lines in .obs file and write that to temp.obs
 sed '/^[ \t]*$/d' $CURDIR/nvsData/$DAYDIR/$OBSFILE > $CURDIR/nvsData/$DAYDIR/temp.obs
-
+# write the temp.obs file to the correct .obs file name
 cat $CURDIR/nvsData/$DAYDIR/temp.obs > $CURDIR/nvsData/$DAYDIR/$OBSFILE
-
+# remove the temp.obs file from the directory
 rm $CURDIR/nvsData/$DAYDIR/temp.obs
+
+# Enable git settings for the build to update git with
+git config --global user.name "GPSdopplarBot"
+git config --global user.email "gpsdopplar@gmail.com"
