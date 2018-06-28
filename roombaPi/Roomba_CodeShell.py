6251f40e85ac147cf59049a853efe3597df9bbfe
Christopher Brant <cbrant@g.clemson.edu>
2018-06-28 13:35:01 -0400

    update

--- roombaPi/Roomba_CodeShell.py
+++ roombaPi/Roomba_CodeShell.py
@@ -77,7 +77,7 @@ if Xbee.inWaiting() > 0: # If anything is in the Xbee receive buffer
 	#print(x) # Include for debugging
 
 # Main Code #
-
+Roomba.PlaySMB()
 
 ## -- Ending Code Starts Here -- ##
 # Make sure this code runs to end the program cleanly
