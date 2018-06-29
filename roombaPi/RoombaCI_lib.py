a5ecb520f7065c6e5d98261215e4d6a32898a730
Christopher Brant <cbrant@g.clemson.edu>
2018-06-29 12:13:06 -0400

    updated note lengths

--- roombaPi/RoombaCI_lib.py
+++ roombaPi/RoombaCI_lib.py
@@ -629,7 +629,7 @@ class Create_2:
 	def WriteMarioDeath1(self):
 		# Define Death Jingle Part 1
 		self.conn.write(b'\x8c\x00\x04') # 140, 0, 4
-		self.conn.write(b'\x48\x04\x49\x04\x4a\x08\x1e\x30')
+		self.conn.write(b'\x48\x02\x49\x02\x4a\x04\x1e\x18')
 		#				    72,  t, 73,  t, 74,  s, 30, q.
 		time.sleep(0.05)
 		# This piece will need 1.025 seconds of time.sleep to run
@@ -637,15 +637,15 @@ class Create_2:
 	def WriteMarioDeath2(self):
 		# Define Death Jingle Part 2
 		self.conn.write(b'\x8c\x01\x0a') # 140, 1, 10
-		self.conn.write(b'\x47\x10\x4d\x10\x1e\x10\x4d\x10\x4d\x10\x1e\x10\x4c\x10\x1e\x10\x4a\x10\x1e\x10')
+		self.conn.write(b'\x47\x08\x4d\x08\x1e\x08\x4d\x08\x4d\x08\x1e\x08\x4c\x08\x1e\x08\x4a\x08\x1e\x08')
 		#					71,  e, 77,  e, 30,  e, 77,  e, 77,  e, 30,  e, 76,  e, 30,  e, 74,  e, 30,  e
 		time.sleep(0.05)
-		# This piece will need 2.05 seconds of time.sleep to run
+		# This piece will need 1.025 seconds of time.sleep to run
 
 	def WriteMarioDeath3(self):
 		# Define Death Jingle Part 3
 		self.conn.write(b'\x8c\x02\x05') # 140, 2, 5
-		self.conn.write(b'\x48\x10\x40\x10\x1e\x10\x40\x10\x3c\x10')
+		self.conn.write(b'\x48\x08\x40\x08\x1e\x08\x40\x08\x3c\x08')
 		#					72,  e, 64,  e, 30,  e, 64,  e, 60,  e
 		time.sleep(0.05)
 		# This piece will need 1.30 seconds of time.sleep to run
@@ -659,14 +659,14 @@ class Create_2:
 		# Play each section with correct timing
 		# MarioDeath1
 		self.conn.write(b'\x8d\x00')
-		time.sleep(1.025)
+		time.sleep(0.525)
 		# MarioDeath2
 		self.conn.write(b'\x8d\x01')
-		time.sleep(2.05)
+		time.sleep(1.30)
 		# MarioDeath3
 		self.conn.write(b'\x8d\x02')
-		time.sleep(1.30)
-		
+		time.sleep(0.65)
+
 
 
 
