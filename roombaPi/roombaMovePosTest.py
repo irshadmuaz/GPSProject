e0fef43e508ab6b4f1d733654e9e715faf0ae7b0
Christopher Brant <cbrant@g.clemson.edu>
2018-07-03 12:34:20 -0400

    updates

--- roombaPi/roombaMovePosTest.py
+++ roombaPi/roombaMovePosTest.py
@@ -139,8 +139,8 @@ while True:
 		# Request for the desired angle to turn to
 		# Print current angle of Roomba
 		print("Current Location: ({0:.3f}, {1:.3f})".format(x_pos, y_pos))
-		print("Current heading is {0:.3f}\n", angle)
-		desired_heading = float(input("Desired heading? "))
+		print("Current heading is {0:.3f}", angle)
+		desired_heading = float(input("\nDesired heading? "))
 		desired_distance = 0
 
 		data_time = 0.0 # 0 seconds initial
