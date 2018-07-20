1186c1e9143fb24174e1ded7979d0593d6fd4750
Christopher Brant <cbrant@g.clemson.edu>
2018-07-20 13:56:36 -0400

    updates

--- PyVMU/calibrate.py
+++ PyVMU/calibrate.py
@@ -6,6 +6,10 @@ import dato
 d = vmu.vmu931("/dev/ttyACM0")
 d.setup()
 d.isConnected()
+d.streamingAccelerometers("Accelerometers")
+d.streamingGyroscopes("Gyroscopes")
+d.streamingHeading("Heading")
+d.streamingMagnetometers("Magnetometers")
 # Now calibrate
 d.calibrate()
 d.close()
