f01e9d3bd22cc80dd9f73723a36436517d90e1b0
Christopher Brant <cbrant@g.clemson.edu>
2018-07-31 13:40:46 -0400

    updates

--- PyVMU/vmu_velocity.py
+++ PyVMU/vmu_velocity.py
@@ -84,9 +84,9 @@ with VMU931Parser(euler=True, accelerometer=True) as vp:
 
 			# Calculate acceleration and velocity here based on the new ax,ay,az values
 			dt = (ts_next - ts_last) * TIME_CONSTANT
-			velo[0] = (acc_inertial[0] * dt) + velo[0]
-			velo[1] = (acc_inertial[1] * dt) + velo[1]
-			velo[2] = (acc_inertial[2] * dt) + velo[2]
+			velo[0] += (acc_inertial[0] * dt)
+			velo[1] += (acc_inertial[1] * dt)
+			velo[2] += (acc_inertial[2] * dt)
 			#print("Velocity in m/s X:{0:0.3f},Y:{1:0.3f},Z:{2:0.3f}".format(velo[0],velo[1],velo[2]))
 			print(dt, ts_next, ts_last)
 
